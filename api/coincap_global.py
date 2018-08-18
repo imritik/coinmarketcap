c1287b17836eda811cf4d1032e43069c748ce5e0
imritik <ritikvverma@gmail.com>
2018-08-18 15:17:08 +0530

    initial commit

---
+++ api/coincap_global.py
@@ -0,0 +1,35 @@
+import json
+import requests
+from datetime import datetime
+
+global_url = ' https://api.coinmarketcap.com/v2/global/'
+request = requests.get(global_url)
+results = request.json()
+
+# print(json.dumps(results,sort_keys=True,indent=4))
+
+active_curriencies = results['data']['active_cryptocurrencies']
+active_markets = results['data']['active_markets']
+bitcoin_percentage = results['data']['bitcoin_percentage_of_market_cap']
+last_updated = results['data']['last_updated']
+global_cap = int(results['data']['quotes']['USD']['total_market_cap'])
+global_volume = int(results['data']['quotes']['USD']['total_volume_24h'])
+
+active_currencies_string = '{:,}'.format(active_curriencies)
+active_markets_string = '{:,}'.format(active_markets)
+global_cap_string = '{:,}'.format(global_cap)
+global_volume_string = '{:,}'.format(global_volume)
+
+
+last_updated_string = datetime.fromtimestamp(last_updated).strftime('%B %d,%Y at %I:%M%p')
+
+print()
+print('There are currently '+ active_currencies_string + ' active cryptocurrencies and ' + active_markets_string + ' active markets.')
+print('The global cap of all cryptos is ' + global_cap_string + ' and the 24h global volume is ' + global_volume_string + '.')
+print('Bitcoins\'s total percentage of the global cap is ' + str(bitcoin_percentage) + '%.')
+print()
+print('This information was last updated on ' + last_updated_string + '.' )
+
+
+
+
---
+++ api/coincap_listing.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+
+listing_url = ' https://api.coinmarketcap.com/v2/listings/'
+
+request = requests.get(listing_url)
+results = request.json()
+
+
+# print(json.dumps(results,sort_keys=True,indent=4))
+
+
+data = results['data']
+
+for currency in data:
+    rank = currency['id']
+    name = currency['name']
+    symbol = currency['symbol']
+    print(str(rank) + ': ' + name + ' (' + symbol + ') ')
